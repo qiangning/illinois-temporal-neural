8110506a70c34fe40aaa7ed5d45d772e3d1fc55a
Sanjay Subramanian <sssubra2@dickens.cs.illinois.edu>
2018-12-05 16:04:47 -0600

    Adding minibatch support and a few other things

--- exp_myLSTM.py
+++ exp_myLSTM.py
@@ -23,9 +23,10 @@ from myLSTM import *
 from Baseline_LSTM import *
 from extract_bigram_stats import temporal_bigram
 from pairwise_ffnn_pytorch import VerbNet
+from language_model import BieberLSTM
 
 class experiment:
-    def __init__(self,model,trainset,testset,output_labels,params,exp_name,modelPath,skiptuning):
+    def __init__(self,model,trainset,testset,output_labels,params,exp_name,modelPath,skiptuning, batch_size):
         self.model = model
         self.params = params
         self.trainset, self.devset = self.split_train_dev(trainset)
@@ -34,6 +35,7 @@ class experiment:
         self.exp_name = exp_name
         self.modelPath = "%s_%s" %(modelPath,self.exp_name)
         self.skiptuning = skiptuning
+        self.batch_size = batch_size
 
     def split_train_dev(self,trainset):
         train,dev = train_test_split(trainset,test_size=0.2,random_state=self.params.get('seed',2093))
@@ -67,6 +69,7 @@ class experiment:
         step_size = self.params.get('step_size',10)
         gamma = self.params.get('gamma',0.3)
         optimizer = optim.SGD(self.model.parameters(), lr=lr, weight_decay=weight_decay)
+        # optimizer = optim.Adam(self.model.parameters(), lr=lr)
         scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
         criterion = nn.CrossEntropyLoss()
         all_train_losses = []
@@ -79,16 +82,22 @@ class experiment:
             current_train_loss = 0
             random.shuffle(trainset)
             scheduler.step()
+            batch_loss = 0.0
+            self.model.zero_grad()
             for i,temprel in enumerate(trainset):
-                self.model.zero_grad()
+                # self.model.zero_grad()
                 target = torch.cuda.LongTensor([self.output_labels[temprel.label]])
                 output = self.model(temprel)
                 loss = criterion(output, target)
                 current_train_loss += loss
+                batch_loss += loss
                 if i % 1000 == 0:
                     print("%d/%d: %s %.4f %.4f" % (i, len(trainset), timeSince(start), loss, current_train_loss), flush=True)
-                loss.backward()
-                optimizer.step()
+                if (i+1) % self.batch_size == 0:
+                    batch_loss.backward()
+                    optimizer.step()
+                    self.model.zero_grad()
+                    batch_loss = 0.0
             all_train_losses.append(current_train_loss)
             current_train_acc, _ = self.eval(trainset)
             current_test_acc, confusion = self.eval(testset)
@@ -187,6 +196,27 @@ class bigramGetter_fromNN:
             return torch.zeros_like(self.model.retrieveEmbeddings(torch.from_numpy(np.array([[0,0]])).cuda()).view(1,-1))
         return self.model.retrieveEmbeddings(torch.from_numpy(np.array([[self.verb_i_map[v1],self.verb_i_map[v2]]])).cuda()).view(1,-1)
 
+class bigramGetter_fromLM:
+    def __init__(self,mdl_path,all_verbs,emb_size=200):
+        self.verb_i_map = {}
+        for i in range(len(all_verbs)):
+            self.verb_i_map[all_verbs[i]] = i
+        self.model = BieberLSTM(len(all_verbs), nb_layers=1)
+        checkpoint = torch.load(mdl_path)
+        self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.emb_size = emb_size
+    def retrieveEmbeddings(self,temprel):
+        v1, v2 = '', ''
+        for i, position in enumerate(temprel.position):
+            if position == 'E1':
+                v1 = temprel.lemma[i]
+            elif position == 'E2':
+                v2 = temprel.lemma[i]
+                break
+        if v1 not in self.verb_i_map or v2 not in self.verb_i_map:
+            return torch.zeros((1, 2*self.emb_size), dtype=torch.float32)
+        return torch.cat(self.model.word_embedding(torch.tensor([self.verb_i_map[v1]]).cuda()), self.model.word_embedding(torch.tensor([self.verb_i_map[v2]]).cuda()), dim=1)
+
 @click.command()
 @click.option("--w2v_option",default=2)
 @click.option("--lstm_hid_dim",default=128)
@@ -204,43 +234,44 @@ class bigramGetter_fromNN:
 @click.option("--dropout", is_flag=True)
 @click.option("--timeline_kb", is_flag=True)
 @click.option("--debug",is_flag=True)
-def run(w2v_option, lstm_hid_dim, nn_hid_dim, pos_emb_dim, common_sense_emb_dim, lr, weight_decay, step_size, gamma, max_epoch, expname, skiptuning, mode, dropout, timeline_kb, debug):
+@click.option("--batch_size", default=1)
+def run(w2v_option, lstm_hid_dim, nn_hid_dim, pos_emb_dim, common_sense_emb_dim, lr, weight_decay, step_size, gamma, max_epoch, expname, skiptuning, mode, dropout, timeline_kb, debug, batch_size):
 
-    trainset = temprel_set("data/Output4LSTM_Baseline/trainset.xml")
-    testset = temprel_set("data/Output4LSTM_Baseline/testset.xml")
+    trainset = temprel_set("data/Output4LSTM_Baseline/trainset-temprel.xml")
+    testset = temprel_set("data/Output4LSTM_Baseline/testset-temprel.xml")
 
     if w2v_option == 0:
         embedding_dim = 256
         print("Using ELMo (small)")
-        emb_cache = elmo_cache(None, "ser/elmo_cache_small.pkl", verbose=False)
+        emb_cache = elmo_cache(None, "/shared/preprocessed/qning2/temporal/ser/elmo_cache_small.pkl", verbose=False)
     elif w2v_option == 1:
         embedding_dim = 512
         print("Using ELMo (medium)")
-        emb_cache = elmo_cache(None, "ser/elmo_cache_medium.pkl", verbose=False)
+        emb_cache = elmo_cache(None, "/shared/preprocessed/qning2/temporal/ser/elmo_cache_medium.pkl", verbose=False)
     elif w2v_option == 2:
         embedding_dim = 1024
         print("Using ELMo (original)")
-        emb_cache = elmo_cache(None, "ser/elmo_cache_original.pkl", verbose=False)
+        emb_cache = elmo_cache(None, "/shared/preprocessed/qning2/temporal/ser/elmo_cache_original.pkl", verbose=False)
     elif w2v_option == 3:
         embedding_dim = 300
         print("glove.6B.300d-light.magnitude")
-        emb_cache = w2v_cache(None, "ser/w2v_cache_glove.6B.300d-light.magnitude.pkl", verbose=False)
+        emb_cache = w2v_cache(None, "/shared/preprocessed/qning2/temporal/ser/w2v_cache_glove.6B.300d-light.magnitude.pkl", verbose=False)
     elif w2v_option == 4:
         embedding_dim = 300
         print("glove.6B.300d-medium.magnitude")
-        emb_cache = w2v_cache(None, "ser/w2v_cache_glove.6B.300d-medium.magnitude.pkl", verbose=False)
+        emb_cache = w2v_cache(None, "/shared/preprocessed/qning2/temporal/ser/w2v_cache_glove.6B.300d-medium.magnitude.pkl", verbose=False)
     elif w2v_option == 5:
         embedding_dim = 300
         print("wiki-news-300d-1M-medium.magnitude")
-        emb_cache = w2v_cache(None, "ser/w2v_cache_wiki-news-300d-1M-medium.magnitude.pkl", verbose=False)
+        emb_cache = w2v_cache(None, "/shared/preprocessed/qning2/temporal/ser/w2v_cache_wiki-news-300d-1M-medium.magnitude.pkl", verbose=False)
     elif w2v_option == 6:
         embedding_dim = 300
         print("GoogleNews-vectors-negative300-medium.magnitude")
-        emb_cache = w2v_cache(None, "ser/w2v_cache_GoogleNews-vectors-negative300-medium.magnitude.pkl", verbose=False)
+        emb_cache = w2v_cache(None, "/shared/preprocessed/qning2/temporal/ser/w2v_cache_GoogleNews-vectors-negative300-medium.magnitude.pkl", verbose=False)
     else:
         print("word embedding option is wrong (%d)." % w2v_option)
         embedding_dim = 256
-        emb_cache = elmo_cache(None, "ser/elmo_cache_small.pkl", verbose=False)
+        emb_cache = elmo_cache(None, "/shared/preprocessed/qning2/temporal/ser/elmo_cache_small.pkl", verbose=False)
 
     position2ix = {"B":0,"M":1,"A":2,"E1":3,"E2":4}
     output_labels = {"BEFORE":0,"AFTER":1,"EQUAL":2,"VAGUE":3}
@@ -402,17 +433,28 @@ def run(w2v_option, lstm_hid_dim, nn_hid_dim, pos_emb_dim, common_sense_emb_dim,
         print("ratio=%s,emb_size=%d,layer=%d,lemma_emb_dim=%d" % (str(ratio), emb_size, layer, params['lemma_emb_dim']))
         bigramGetter = bigramGetter_fromNN(emb_path, mdl_path, ratio, layer, emb_size, splitter=',')
         model = lstm_NN_bigramStats4(params, emb_cache, bigramGetter, position2ix, granularity=0.1, common_sense_emb_dim=common_sense_emb_dim)
+    elif mode == 14: # Proposed: with embeddings from LM but put one extra layer after embeddings before concat with the final layer of output
+        hidden_size = 100
+        emb_size = 200
+        print("---------")
+        all_verbs_path = '/shared/preprocessed/sssubra2/embeddings/models/LanguageModel/all_verbs.npy'
+        all_verbs = np.load(all_verbs_path)
+        mdl_path = '/shared/preprocessed/sssubra2/embeddings/models/LanguageModel/language_model_emb%d_bilstm%d.pt' % (
+            emb_size, hidden_size)
+        params['lemma_emb_dim'] = int(emb_size*2)
+        bigramGetter = bigramGetter_fromLM(mdl_path, all_verbs, emb_size)
+        model = lstm_NN_embeddings2(params, emb_cache, bigramGetter, position2ix)
     else:
         print('Error! No such mode: %d' %mode)
         sys.exit(-1)
     if debug:
         exp = experiment(model=model, trainset=trainset.temprel_ee[:100], testset=testset.temprel_ee[:100], \
                          params=params_optim, exp_name=expname, modelPath="models/ckpt", \
-                         output_labels=output_labels, skiptuning=skiptuning)
+                         output_labels=output_labels, skiptuning=skiptuning,batch_size=batch_size)
     else:
         exp = experiment(model=model,trainset=trainset.temprel_ee,testset=testset.temprel_ee,\
                          params=params_optim,exp_name=expname,modelPath="models/ckpt", \
-                         output_labels=output_labels,skiptuning=skiptuning)
+                         output_labels=output_labels,skiptuning=skiptuning,batch_size=batch_size)
     exp.train()
     exp.test()
 
